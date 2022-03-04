d5fb70c94acfbbe48fb54f10e70d4150bcd5e313
Eggnigma <98013904+Eggnigma@users.noreply.github.com>
2022-03-04 15:52:11 -0500

    Create ListIV.py

---
+++ ListIV.py
@@ -0,0 +1,12 @@
+lst1=[4, 10, 2, 5]
+lst2=[-10, 2, 5, 10]
+
+def larger_list(lst1,lst2):
+  if len(lst1) > len(lst2):
+    return lst1[-1]
+  elif len(lst2) > len(lst1):
+    return lst2[-1]
+  else:
+    return lst1[-1]
+
+print(larger_list(lst1,lst2))
